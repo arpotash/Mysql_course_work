3b553dae9b2fc751a745e06101c4da2f453029d8
Artem Potashov <artem.potashov@bk.ru>
2020-02-29 05:48:38 +0300

    Добавил описание процедуры

--- Procedure.sql
+++ Procedure.sql
@@ -1,3 +1,5 @@
+-- процесс покупки пользователем услуги со снятием средств с платежной карты
+
 delimiter //
 
 drop procedure if exists payment_process//
@@ -5,11 +7,16 @@ create procedure payment_process(in payer_id int, in service_id int, in cart_id 
 begin
 	set @money = (select amount from payment_cart where id = cart_id);
 	set @price = (select price from services where id = service_id);
+	set @discout = (select discount from service where id = service_id);
 	if (@money >= @price) then
-		update payment_cart set amount = amount - @price where id = cart_id;
+		update payment_cart set amount = amount - (@price - @price*@discout/100) where id = cart_id;
 		insert into payment(user_id, services_id, payment_cart_id , create_pay) 
 		values (payer_id, service_id, cart_id, now());
 	else
 		signal sqlstate 'HY008' SET Message_text = 'Недостаточно средств на карте!';
 	end if;
 end//
+
+
+
+
