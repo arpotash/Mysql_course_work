f6202bec3d27c9c26f2d1ddac26138b7fa97cd62
Artem Potashov <artem.potashov@bk.ru>
2020-03-02 02:52:07 +0300

    Оптимизация оплаты

--- Procedure.sql
+++ Procedure.sql
@@ -1,21 +1,23 @@
 -- процесс покупки пользователем услуги со снятием средств с платежной карты
 
 delimiter //
-
 drop procedure if exists payment_process//
-create procedure payment_process(in payer_id int, in service_id int, in cart_id int)
+create procedure payment_process(in service_id int, in cart_id int)
 begin
 	set @money = (select amount from payment_cart where id = cart_id);
 	set @price = (select price from services where id = service_id);
-	set @discout = (select discount from service where id = service_id);
+	set @discout = (select discount from services where id = service_id);
+	set @payer_id = (select user_id from payment_cart where id = cart_id);
 	if (@money >= @price) then
-		update payment_cart set amount = amount - (@price - @price*@discout/100) where id = cart_id;
+		update payment_cart set amount = amount - (@price - (@price*@discout/100)) where id = cart_id;
 		insert into payment(user_id, services_id, payment_cart_id , create_pay) 
-		values (payer_id, service_id, cart_id, now());
+		values (@payer_id, service_id, cart_id, now());
 	else
 		signal sqlstate 'HY008' SET Message_text = 'Недостаточно средств на карте!';
 	end if;
 end//
+
+
 
 
 
